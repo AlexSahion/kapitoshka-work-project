da570a1929b5801dade52ca5330d6dbd71eaa8e1
AlexSahion <saha.las399@gmail.com>
2024-04-27 11:52:46 +0200

    add: burger

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-//Burger Menu
-// const iconMenu = document.querySelector(".menu__icon")
-// const menuBody = document.querySelector(".menu__body")
-// if (iconMenu) {
-//   iconMenu.addEventListener('click', e => {
-//     document.body.classList.toggle("_active")
-//     iconMenu.classList.toggle("_active")
-//     menuBody.classList.toggle("_active")
-//   })
-// }
+const body = document.body
+const iconMenu = document.querySelector(".menu__icon")
+const menuBody = document.querySelector(".menu__body")
+if (iconMenu) {
+	iconMenu.addEventListener('click', e => {
+		body.classList.toggle("_lock")
+		iconMenu.classList.toggle("_active")
+		menuBody.classList.toggle("_active")
+	})
+}
