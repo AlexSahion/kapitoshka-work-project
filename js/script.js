afcf7b4b0eb070ee75018322de236945f13a4290
AlexSahion <saha.las399@gmail.com>
2024-10-01 13:12:25 +0200

    fix: apdate

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-document.addEventListener('click', e => {
-	if (document.querySelector('.open') && !e.target.closest('popup__body')) {
-		popupClose(popup)
-	}
-	if (e.target.closest('._popup')) {
-		const link = e.target.closest('._popup')
-		const wayToPopup = link.getAttribute("href").split('#')[1]
-		const popup = document.getElementById(wayToPopup)
-
-		popupOpen(popup)
-	}
-
-	if (e.target.closest('._popup-close')) {
-		popupClose(popup)
-	}
-
-})
-
-function popupOpen(popup) {
-	popup.classList.add('open')
-}
-
-function popupClose(popup) {
-	popup.classList.remove('open')
-}
-
-
-
 new Swiper('.cost__swiper', {
 	loop: true,
 	spaceBetween: 30,
